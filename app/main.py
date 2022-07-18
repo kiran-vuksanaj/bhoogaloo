7814f665093ea1f1b3c9f1b665dba8f1d184721d
Kiran Vuksanaj <kiranv@mit.edu>
2022-07-18 15:44:07 -0400

    working draggable queue

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import json
 
 app = FastAPI()
 
@@ -11,26 +12,86 @@ app = FastAPI()
 #         }
 #     return {"data":data}
 
+INITIAL_VOLUME = 20
+
+class Song:
+    def __init__(self,obj):
+        self.id = obj.id
+        self.start = obj.start
+        self.end = obj.end
+
 class PlayerState:
     def __init__(self):
-        self.n = 4
+        self.vol = INITIAL_VOLUME
+        self.queue = []
+    
+    def update_state(self,json_cmd):
+        cmd_type = json_cmd['cmd']
+        CMD_MAP = {
+            'VOL_UP': self.vol_up,
+            'VOL_DN': self.vol_dn,
+            'MUTE': self.mute,
+            'ENQUEUE': self.enqueue,
+            'QSHIFT': self.qshift
+        }
+        CMD_MAP[cmd_type](json_cmd)
+
+    # COMMAND EXECUTIONS    
+    
+    def vol_up(self,cmd):
+        self.vol += 5
+    
+    def vol_dn(self,cmd):
+        self.vol -= 5
+    
+    def mute(self,cmd):
+        self.vol = 0
+
+    def enqueue(self,cmd):
+        new_song = cmd['song']
+        self.queue.append(new_song)
+
+    def qshift(self,cmd):
+        song = cmd['song']
+        idx = cmd['idx']
+        self.queue.remove(song)
+        self.queue.insert(idx,song)
+
+    def to_json(self):
+        return json.dumps({
+            'vol':self.vol,
+            'queue':self.queue
+        })
+
+class SocketBoard:
+    def __init__(self):
+        self.state = PlayerState()
         self.connections = []
     
-    def connect(self,websocket):
+    async def send_state(self,websocket):
+        print('sending state')
+        await websocket.send_text(self.state.to_json())
+
+    async def connect(self,websocket):
         self.connections.append(websocket)
+        await self.send_state(websocket)
         return True
     
     def disconnect(self,websocket):
         self.connections.remove(websocket)
-
-    async def update_state(self,n):
-        self.n = n
-        for websocket in self.connections:
-            await websocket.send_text(f'new n: {n}')
         return True
 
+    async def update_state(self,cmd):
+        try:
+            self.state.update_state(json.loads(cmd))
+            for websocket in self.connections:
+                await self.send_state(websocket)
+            return True
+        except:
+            return False
 
-playerstate = PlayerState()
+
+socketboard = SocketBoard()
 
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
@@ -45,11 +106,13 @@ async def websocket_client(websocket: WebSocket):
     print('hi')
     await websocket.accept()
     
-    playerstate.connect(websocket)
+    await socketboard.connect(websocket)
 
     try:
         while True:
-            data = await websocket.receive_text()
-            await playerstate.update_state(data)
+            cmd = await websocket.receive_text()
+            successful_update = await socketboard.update_state(cmd)
+            if not successful_update:
+                websocket.send_text
     except WebSocketDisconnect:
-        await playerstate.disconnect(websocket)
+        await socketboard.disconnect(websocket)
--- app/static/index.js
+++ app/static/index.js
@@ -1,33 +1,70 @@
 const ws = new WebSocket("ws://localhost:8080/ws/controller", "echo-protocol");
 const inputForm = document.getElementById("inputForm");
-const messages = document.getElementById("messages");
+const queue = document.getElementById("queue");
+const volDn = document.getElementById('volDn');
+const volUp = document.getElementById('volUp');
+
+const enqueueForm = document.getElementById('enqueueForm');
+
+function displayQueue(songs) {
+    while(queue.firstChild) {
+        queue.removeChild(queue.firstChild);
+    }
+    for(const song of songs) {
+        const queueItem = document.createElement('li');
+        const content = document.createTextNode(song);
+        queueItem.className = 'list-group-item';
+        queueItem.appendChild(content);
+        queue.appendChild(queueItem);
+    }
+}
+
+function displayState(state){
+    console.log(state);
+    const volume = document.getElementById('volume');
+    // volume.appendChild(
+    //     document.createTextNode(`${state['vol']}`)
+    // );
+    volume.innerText = `${state['vol']}`
+    displayQueue(state['queue']);
+}
 
 ws.onmessage = function(event) {
-    var messages = document.getElementById('messages')
-    var message = document.createElement('li')
-    var content = document.createTextNode(event.data)
-    message.className = 'list-group-item';
-    console.log(message);
-    message.appendChild(content)
-    messages.appendChild(message)
+
+    try {
+        const state = JSON.parse(event.data);
+        displayState(state);
+    } catch {
+        console.log('not json');
+    }
 };
-function sendMessage(event) {
+function sendMessage(event,cmd) {
     event.preventDefault();
-    var input = document.getElementById("messageText")
-    ws.send(input.value)
-    input.value = ''
-    event.preventDefault()
+    ws.send(JSON.stringify(cmd));
 }
-inputForm.addEventListener('submit',sendMessage);
-// inputForm.onsubmit(sendMessage);
+
+volDn.addEventListener('click',(event) => {sendMessage(event,{'cmd':'VOL_DN'})});
+volUp.addEventListener('click',(event) => {sendMessage(event,{'cmd':'VOL_UP'})});
+
+function sendEnqueue(event) {
+    event.preventDefault();
+    enqueueId = document.getElementById('enqueueId').value;
+    sendMessage(event,{'cmd':'ENQUEUE','song':enqueueId});
+}
+enqueueForm.addEventListener('submit',sendEnqueue);
 
 function sendResorting(event) {
-    ws.send(`move ${event.oldIndex} to ${event.newIndex}`);
+    // ws.send(`move ${event.oldIndex} to ${event.newIndex}`);
+    sendMessage(event,{
+        'cmd':'QSHIFT',
+        'song':event.item.innerText,
+        'idx':event.newIndex
+    })
 }
 
-Sortable.create(messages, {
+Sortable.create(queue, {
     animation: 100, 
-    group: 'messages', 
+    group: 'queue', 
     draggable: '.list-group-item', 
     handle: '.list-group-item', 
     sort: true, 
